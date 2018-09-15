d09b9845f32f3905247dafefc92bdcc412f75761
SergLam <lamcevs0@gmail.com>
2018-09-15 20:48:32 +0300

    Finish sign up function(tested)

---
+++ functions/auth/sign_up.f.js
@@ -0,0 +1,99 @@
+const init = require('../init_app.js');
+const functions = require('firebase-functions');
+const admin = init.init_app();
+const firestore = admin.firestore();
+const helper = require('../helpers/helper.js');
+
+module.exports = functions.https.onRequest((req, res) => {
+  var email = "";
+  var password = "";
+  const usersRef = firestore.collection('users');
+
+  if (req.body.hasOwnProperty("email")) {
+    email = req.body.email;
+  } else {
+    return res.status(400).send({ error: "email field is missing" });
+  }
+
+  if (req.body.hasOwnProperty("password")) {
+    password = req.body.password;
+  } else {
+    return res.status(400).send({ error: "password field is missing" });
+  }
+
+  // Validate email
+  var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(regex.test(email)){
+    var user = ""
+    // Search by email in collection
+    var promise = usersRef.where('email', '==', email).get()
+      .then(snapshot => {
+      snapshot.forEach(doc => {
+        if(doc.data().email === email){
+          user = doc.data();
+        }
+      });
+      return user;
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+      return res.status(400).send({ error: "firebase internal error" });
+    });
+      // RETURN RESULT AFTER THEN (OR THROW)!!
+      if(user === ""){
+        // TODO: Add user to users collection
+
+        const createdAt = helper.is_undefined(req.body.createdAt);
+        const email = helper.is_undefined(req.body.email);
+        const firstName = helper.is_undefined(req.body.firstName);
+        const lastName = helper.is_undefined(req.body.lastName);
+        const displayName = helper.is_undefined(req.body.displayName);
+        const photoURL = helper.is_undefined(req.body.photoURL);
+        const lastSignInTime = helper.is_undefined(req.body.lastSignInTime);
+        const longitude = helper.is_undefined(req.body.location.longitude);
+        const latitude = helper.is_undefined(req.body.location.latitude);
+
+        // Add a new document with a generated id.
+        var newUserRef = usersRef.doc();
+        const new_user = {
+           uid: newUserRef.id,
+           createdAt: createdAt,
+           email: email,
+           displayName: displayName,
+           photoURL: photoURL,
+           lastSignInTime: lastSignInTime,
+           notification_tokens: {
+             token: true
+           }
+         };
+        // Add user to auth table
+        admin.auth().createUser({
+          uid: newUserRef.id,
+          email: email,
+          emailVerified: false,
+          password: password,
+          displayName: displayName,
+          photoURL: photoURL,
+          disabled: false
+        })
+        .then(function(userRecord) {
+          // See the UserRecord reference doc for the contents of userRecord.
+          console.log("Successfully created new user:", userRecord.uid);
+          var setDoc = newUserRef.set(
+             new_user
+            ).then(ref => {
+              console.log('Added user with ID: ', newUserRef.id);
+              return res.status(200).send(new_user);
+            });
+            return res.status(200).send(new_user);
+        })
+        .catch(function(error) {
+          console.log("Error creating new user:", error);
+        });
+      } else{
+        return res.status(400).send({ message: "email already registered" });
+      }
+  } else {
+    return res.status(400).send({ message: "invalid email" });
+  }
+});
--- functions/users/update_user.f.js
+++ functions/users/update_user.f.js
@@ -6,58 +6,59 @@ const helper = require('../helpers/helper.js');
 
 module.exports = functions.https.onRequest((req, res) => {
 
-var uid = "";
-var user = {};
+var email = "";
 
-if (req.body.hasOwnProperty("uid")) {
-  uid = req.body.uid;
+if (req.body.hasOwnProperty("email")) {
+  email = req.body.email;
 } else {
-  return res.status(400).send({ error: "uid field is missing" });
-}
-
-if (req.body.hasOwnProperty("user")) {
-  user = req.body.user;
-} else {
-  return res.status(400).send({ error: "user object is missing" });
-}
-
-if (req.body.user.hasOwnProperty("email")) {
-  user["email"] = req.body.user.email;
-} else {
-  return res.status(400).send({ error: "user email is missing" });
+  return res.status(400).send({ error: "email field is missing" });
 }
 
 const users = firestore.collection('users');
+var isUpdated = false;
+var updatedUser = {};
 
-var getDoc = users.doc(uid).get()
-    .then(doc => {
-        if (!doc.exists) {
-            return res.status(400).send({ error: "no such user" });
-        } else {
-          const createdAt = helper.is_undefined(user.createdAt);
-          const email = helper.is_undefined(user.email);
-          const displayName = helper.is_undefined(user.displayName);
-          const photoURL = helper.is_undefined(user.photoURL);
-          const lastSignInTime = helper.is_undefined(user.lastSignInTime);
-          const longitude = helper.is_undefined(user.location.longitude);
-          const latitude = helper.is_undefined(user.location.latitude);
+var promise = users.where('email', '==', email).get()
+  .then(snapshot => {
+  snapshot.forEach(doc => {
+    if(doc.data().email === email){
+      const createdAt = helper.is_undefined(req.body.createdAt);
+      const email = helper.is_undefined(req.body.email);
+      const firstName = helper.is_undefined(req.body.firstName);
+      const lastName = helper.is_undefined(req.body.lastName);
+      const displayName = helper.is_undefined(req.body.displayName);
+      const photoURL = helper.is_undefined(req.body.photoURL);
+      const lastSignInTime = helper.is_undefined(req.body.lastSignInTime);
+      const longitude = helper.is_undefined(req.body.location.longitude);
+      const latitude = helper.is_undefined(req.body.location.latitude);
 
-          users.doc(uid).set({
-              uid: uid,
-              createdAt: createdAt,
-              email: email,
-              displayName: displayName,
-              photoURL: photoURL,
-              lastSignInTime: lastSignInTime,
-              location: {
-                latitude: latitude,
-                longitude: longitude
-              }
-            });
-          return res.status(200).send({ message: "user updated" });
-        }
-    })
-    .catch(err => {
-        return console.log('Error getting document', err);
-    });
+      users.doc(doc.id).update({
+          createdAt: createdAt,
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+          displayName: displayName,
+          photoURL: photoURL,
+          lastSignInTime: lastSignInTime,
+          location: {
+            latitude: latitude,
+            longitude: longitude
+          }
+        });
+        
+        isUpdated = true;
+        updatedUser = doc.data();
+    }
+  });
+  // RETURN RESULT AFTER THEN (OR THROW)!!
+  if(!isUpdated){
+    return res.status(400).send({ message: "email not registered" });
+  }else{
+    return res.status(200).send(updatedUser);
+  }
+})
+.catch(err => {
+  console.log('Error getting documents', err);
+});
+
 });
